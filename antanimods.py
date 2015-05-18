9a36675ce27a25415b93dd3bcb41e5ed12101fb0
Radu Andries <me@admiral0.it>
2015-05-18 21:50:42 +0200

    Fix imports

--- antanimods.py
+++ antanimods.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-from ModRepository import Util
+from antanirepos import Util
 
 Util.init()
--- antanirepos/__init__.py
+++ antanirepos/__init__.py
@@ -1,7 +1,7 @@
 __author__ = 'admiral0'
 
 from .ModRepository import ModRepository
+from .PackRepository import PackRepository
 from .Mod import Mod
-from .Exceptions import ModDoesNotExistInRepo
 
 
